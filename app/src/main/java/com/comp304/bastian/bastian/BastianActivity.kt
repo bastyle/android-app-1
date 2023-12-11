4ca398840b6bd5287ad1713a43bb682568103462
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-12-11 08:53:29 -0500

    added initial files

---
+++ app/src/main/java/com/comp304/bastian/bastian/BastianActivity.kt
@@ -0,0 +1,11 @@
+package com.comp304.bastian.bastian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BastianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BastianBastias_COMP304_Hands-On_Test2_W23"
+include(":app")
+ 
